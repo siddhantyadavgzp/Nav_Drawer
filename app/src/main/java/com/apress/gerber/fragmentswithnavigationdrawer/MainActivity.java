355488716e9fbcdeaad83bacf3b468a4b4227458
siddhantyadavgzp <42622546+siddhantyadavgzp@users.noreply.github.com>
2018-12-05 07:29:38 +0530

    Initial commit

---
+++ app/src/main/java/com/apress/gerber/fragmentswithnavigationdrawer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.apress.gerber.fragmentswithnavigationdrawer;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar1=findViewById(R.id.toolbar);
+    drawer =findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawer, toolbar1,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+        super.onBackPressed();
+    }
+}
